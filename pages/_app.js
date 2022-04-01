267b41765b75c217eb545e5cd4a68fac1c7db289
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2022-04-01 18:23:07 +0100

    Add landing page

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import { MantineProvider } from "@mantine/core"
 
 function MyApp({ Component, pageProps }) {
-  return <MantineProvider theme={{ colorScheme: 'dark', fontFamily: "Nunito" }}>
+  return <MantineProvider theme={{ colorScheme: 'dark', fontFamily: "Lato" }}>
     <Component {...pageProps} />
   </MantineProvider>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,64 @@
 import Styled from "styled-components";
-import { Button } from "@mantine/core";
+import { AppShell, Header, Text, Button, Group, Box, Card, SimpleGrid, ActionIcon } from "@mantine/core";
+import { Book, AlertTriangle, Calendar } from "tabler-icons-react"
 
-const Root = Styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100vw;
-`;
+function Navbar() {
+  return <Header height={60} p="sm">
+    <Group position="apart">
+      <Text size="xl" gradient={{ from: "indigo", to: "cyan", deg: 45 }} variant="gradient">EDULONK</Text>
+      <Group>
+        <Button>Login</Button>
+        <Button>Register</Button>
+      </Group>
+    </Group>
+  </Header>
+}
+
+function MainCard() {
+  return <Box>
+    <Card>
+      <Text gradient={{ from: "indigo", to: "cyan", deg: 45 }} variant="gradient" style={{ fontSize: 64 }} align="center">Welcome to Edulonk</Text>
+      <Text color="gray" size="xl" align="center" pb="lg">Enhancing Education</Text>
+      <Group position={"center"}>
+        <Button>Login</Button>
+        <Button>Register</Button>
+      </Group>
+    </Card>
+  </Box>
+}
 
 export default function Home() {
   return (
-    <Root>
-      <Button>Ping Harvey</Button>
-    </Root>
+    <AppShell
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+      header={<Navbar />}>
+      <MainCard />
+      <SimpleGrid cols={3} mt="md">
+        <Card>
+          <Group position="center">
+            <Book size={40} color="#FD7E14" />
+            <Text gradient={{ from: "orange", to: "red", deg: 45 }} variant="gradient" style={{ fontSize: 32 }} align="center">Homework</Text>
+            <Text p="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam recusandae sapiente a vel mollitia suscipit aspernatur minima? Minus voluptates soluta facere cupiditate, amet sapiente similique, maiores, natus quos pariatur doloribus.</Text>
+          </Group>
+        </Card>
+        <Card>
+          <Group position="center">
+            <AlertTriangle size={40} color="#82C91E" />
+            <Text gradient={{ from: "lime", to: "cyan", deg: 45 }} variant="gradient" style={{ fontSize: 32 }} align="center">Behaviour</Text>
+            <Text p="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam recusandae sapiente a vel mollitia suscipit aspernatur minima? Minus voluptates soluta facere cupiditate, amet sapiente similique, maiores, natus quos pariatur doloribus.</Text>
+          </Group>
+        </Card>
+        <Card>
+          <Group position="center">
+            <Calendar size={40} color="#845EF7" />
+            <Text gradient={{ from: "violet", to: "pink", deg: 45 }} variant="gradient" style={{ fontSize: 32 }} align="center">Planning</Text>
+          </Group>
+          <Text p="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam recusandae sapiente a vel mollitia suscipit aspernatur minima? Minus voluptates soluta facere cupiditate, amet sapiente similique, maiores, natus quos pariatur doloribus.</Text>
+        </Card>
+      </SimpleGrid>
+
+    </AppShell>
   );
 }
