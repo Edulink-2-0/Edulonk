0e4e1b6ad3d113f3dd8dbf45e43115237ddf3dfe
20c33406 <75366745+20c33406@users.noreply.github.com>
2022-04-02 19:09:39 +0100

    Changed font

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,9 @@ export default class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    {/* Head stuff */}
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link href="https://fonts.googleapis.com/css2?family=Hammersmith+One&display=swap" rel="stylesheet" />                
                 </Head>
                 <body>
                     <Main />
@@ -43,3 +45,4 @@ export default class MyDocument extends Document {
         }
     }
 }
+
